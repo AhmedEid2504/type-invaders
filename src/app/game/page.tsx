067204866db2c72dac61192ccd05b66902902121
AhmedEid2504 <ahmedeid2504@gmail.com>
2024-11-17 19:58:18 +0200

    adds base structure for files of the game

---
+++ src/app/game/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function GamePage() {
+    return (
+        <main className="h-screen flex flex-col">
+            <h1 className="text-4xl font-bold text-center">Welcome to Type Invaders</h1>
+
+        </main>
+    );
+}
